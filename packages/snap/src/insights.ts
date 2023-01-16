4130ec6a13f55d53e9ca9286a2c199171bf3bf78
Bastien <bastien@alchemy.com>
2023-01-16 17:45:16 +0100

    Remove 4byte decoding and add in / out asset changes

--- packages/snap/src/insights.ts
+++ packages/snap/src/insights.ts
@@ -14,7 +14,7 @@ import { decode } from '@metamask/abi-utils';
  * @param transaction - The transaction to get insights for.
  * @returns The transaction insights.
  */
-export async function getInsights(transaction: Record<string, unknown>) {
+export async function getInsights(transaction: Record<string, string>) {
   console.log(transaction);
   try {
     // Check if the transaction has data.
@@ -28,42 +28,27 @@ export async function getInsights(transaction: Record<string, unknown>) {
       };
     }
 
-    const transactionData = remove0x(transaction.data);
-
-    // Get possible function names for the function signature, i.e., the first
-    // 4 bytes of the data.
-    const functionSignature = transactionData.slice(0, 8);
-    const matchingFunctions = await getFunctionsBySignature(
-      add0x(functionSignature),
-    );
-
-    // No functions found for the signature.
-    if (matchingFunctions.length === 0) {
-      return {
-        type: 'Unknown transaction',
-      };
-    }
-
-    // This is a function name in the shape "functionName(arg1Type,arg2Type,...)", so
-    // we do a simple slice to get the argument types.
-    const functionName = matchingFunctions[0];
-    const parameterTypes = functionName
-      .slice(functionName.indexOf('(') + 1, functionName.indexOf(')'))
-      .split(',');
-
-    // Decode the parameters using the ABI utils library.
-    const decodedParameters = decode(
-      parameterTypes,
-      add0x(transactionData.slice(8)),
-    );
-
     // Simulate the transaction
     const { changes, error } = await getSimulationResult(transaction);
 
+    const _out =
+      changes.length > 0
+        ? changes
+            .filter((change) => change.from === transaction.from.toLowerCase())
+            .map((change) => `${change.amount} ${change.symbol}`)
+        : [];
+
+    const _in =
+      changes.length > 0
+        ? changes
+            .filter((change) => change.to === transaction.from.toLowerCase())
+            .map((change) => `${change.amount} ${change.symbol}`)
+        : [];
+
     // Return the function name and decoded parameters.
     return {
-      type: functionName,
-      args: decodedParameters.map(normalize4ByteValue),
+      _out,
+      _in,
       simulation: !error ? changes : error,
     };
   } catch (error) {
@@ -72,80 +57,6 @@ export async function getInsights(transaction: Record<string, unknown>) {
       type: 'Unknown transaction',
     };
   }
-}
-
-/**
- * The ABI decoder returns certain which are not JSON serializable. This
- * function converts them to strings.
- *
- * @param value - The value to convert.
- * @returns The converted value.
- */
-function normalize4ByteValue(value: unknown): unknown {
-  if (Array.isArray(value)) {
-    return value.map(normalize4ByteValue);
-  }
-
-  if (value instanceof Uint8Array) {
-    return bytesToHex(value);
-  }
-
-  if (typeof value === 'bigint') {
-    return value.toString();
-  }
-
-  return value;
-}
-
-// The API endpoint to get a list of functions by 4 byte signature.
-const API_ENDPOINT =
-  'https://www.4byte.directory/api/v1/signatures/?hex_signature=';
-
-/* eslint-disable camelcase */
-type FourByteSignature = {
-  id: number;
-  created_at: string;
-  text_signature: string;
-  hex_signature: string;
-  bytes_signature: string;
-};
-/* eslint-enable camelcase */
-
-/**
- * Gets the function name(s) for the given 4 byte signature.
- *
- * @param signature - The 4 byte signature to get the function name(s) for. This
- * should be a hex string prefixed with '0x'.
- * @returns The function name(s) for the given 4 byte signature, or an empty
- * array if none are found.
- */
-async function getFunctionsBySignature(
-  signature: `0x${string}`,
-): Promise<string[]> {
-  const response = await fetch(`${API_ENDPOINT}${signature}`, {
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `Unable to fetch functions for signature "${signature}": ${response.status} ${response.statusText}.`,
-    );
-  }
-
-  // The response is an array of objects, each with a "text_signature" property.
-  const { results } = (await response.json()) as {
-    results: FourByteSignature[];
-  };
-
-  // The "text_signature" property is a string like "transfer(address,uint256)",
-  // which is what we want. They are sorted by oldest first.
-  // We pick the oldest because it's probably the result that we want.
-  return results
-    .sort((a, b) => b.created_at.localeCompare(a.created_at))
-    .map((result) => result.text_signature);
 }
 
 // The API endpoint to get a list of functions by 4 byte signature.
